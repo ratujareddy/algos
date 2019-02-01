39eefd9d4e2731d38937afe4ab758cf381c30c5d
Ratuja Reddy <rreddy@civisanalytics.com>
2019-01-31 21:09:51 -0600

    add pointers.c

---
+++ pointers/pointers.c
@@ -0,0 +1,18 @@
+/* Ratuja Reddy
+January 31
+
+Refreshing my memory on C!!
+
+Need some poinTERS ha HA HA */
+
+#include <stdio.h> 
+  
+int main() 
+{ 
+    int x; 
+  
+    // Prints address of x 
+    printf("%p", &x); 
+  
+    return 0; 
+} 
